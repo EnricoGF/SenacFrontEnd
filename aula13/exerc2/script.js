03c6caaddb3e713689a353f3a7ce0cc9536c1351
Enrico <enricogaforte@gmail.com>
2024-11-26 11:48:23 -0300

    feat: exercicios aula 13

---
+++ aula13/exerc2/script.js
@@ -0,0 +1,14 @@
+function getName() {
+    let name = document.getElementById("name").value
+    document.getElementById("showNome").innerHTML = name;
+  }
+
+
+
+function myFunction() {
+  // Get the value of the input field with id="numb"
+  let x = document.getElementById("numb").value;
+  // If x is Not a Number or less than one or greater than 10
+  
+  document.getElementById("demo").innerHTML = x;
+}
