95a63d8e0df4dd23dce6eb68c15326aba1efbae0
Enrico <enricogaforte@gmail.com>
2024-12-06 11:48:15 -0300

    fix: aula 21

--- aula21/Form/script.js
+++ aula21/Form/script.js
@@ -1,5 +1,4 @@
 const form = document.getElementById('formulario')
-const msg = document.getElementById('mensagem')
 const campoTexto = document.getElementById('mensagem')
 
 form.addEventListener('submit', function(event){
@@ -7,10 +6,18 @@ form.addEventListener('submit', function(event){
     alert('Formulario enviado')
 })
 
-campoTexto.addEventListener('focus', function(){
-    campoTexto.style.backgroundColor = 'yellow'
+campoTexto.addEventListener('focus', function() {
+    campoTexto.style.backgroundColor = 'rgb(255, 255, 200)'
 })
 
-campoTexto.addEventListener('blur', function(){
+campoTexto.addEventListener('blur', function() {
     campoTexto.style.backgroundColor = 'white'
+})
+
+campoTexto.addEventListener('change', function() {
+    alert('Valor alterado para: ' + campoTexto.value)
+})
+
+window.addEventListener('scroll', function() {
+    console.log('A página foi rolada. Posição do scroll: ' + window.scrollY)
 })
